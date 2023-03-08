3f1977f8ffe5fc228a618f482ae9a963230641b9
MaedehBDNF <badanfirouzm@gmail.com>
2023-03-08 22:30:56 +0330

    complete User class.

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,12 +1,36 @@
-public class User {
-    //User should have a list of books
-    //User should have a username and a password
+import java.util.ArrayList;
 
-    public void rentBook(){
-        //TODO
+public class User extends Account {
+
+    private ArrayList<Book> booksInRentByUser = new ArrayList<>();
+
+    public User(String username, String password){
+        super(username, password);
     }
 
-    public void returnBook(){
-        //TODO
+    public Book[] getBooksInRentByUser() {
+        Book[] result = new Book[booksInRentByUser.size()];
+        return this.booksInRentByUser.toArray(result);
+    }
+
+    public void rentBook(Book book) {
+        book.rentBook();
+        this.booksInRentByUser.add(book);
+    }
+
+    public void returnBook(Book book){
+        if (this.doesRentBook(book)) {
+            book.returnBook();
+            booksInRentByUser.remove(book);
+        }
+    }
+    @Override
+    public User update(AccountEntity accountEntity) {
+        super.update(accountEntity);
+        return this;
+    }
+
+    private boolean doesRentBook(Book book) {
+        return this.booksInRentByUser.contains(book);
     }
 }
